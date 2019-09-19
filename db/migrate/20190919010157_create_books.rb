e5ddd35b588133d83171a14e5fcd939ebe059aff
kaitokwt <vitesthedust@gmail.com>
2019-09-19 16:40:51 +0900

    remove/column/image/books

---
+++ db/migrate/20190919010157_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string     :title, null: false, index: true
+      t.string     :image
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
