fac057ebd046edd8a25e939ceb818b4332729462
kaitokwt <vitesthedust@gmail.com>
2019-09-20 12:10:56 +0900

    edit/click/icon/acodion/menu

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,9 +1,9 @@
 $(function(){
   //.bookの中のp要素がクリックされたら
-  $('.book p').click(function(){
+  $('.book .book__icon').click(function(){
     //クリックされた.bookの中のp要素に隣接する.bookの中の.book__characterを開いたり閉じたりする。
 		$(this).next('.book .book__character').slideToggle();
 		//クリックされた.bookの中のp要素以外の.bookの中のp要素に隣接する.bookの中の.book__characterを閉じる
-		$('.book p').not($(this)).next('.book .book__character').slideUp();
+		$('.book .book__icon').not($(this)).next('.book .book__character').slideUp();
 	});
 });
